89bf0b8fe74a5c93df8275cf097fce2c3a1655e8
zmc0806 <miz030@ucsd.edu>
2024-03-08 16:54:24 -0800

    1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
   import { goto } from '$app/navigation';
 </script>
 
-<button on:click={() => goto('/gdp')}>Go to GDP Page</button>
-<button on:click={() => goto('/gini')}>Go to Gini Page</button>
+<button on:click={() => goto('project/gdp')}>Go to GDP Page</button>
+<button on:click={() => goto('project/gini')}>Go to Gini Page</button>
 
 <style>
   /* Background image styling */
